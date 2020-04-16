9ac200967a20d72cab954b23146c7abf830988e1
GeorgeR <dukecg@gmail.com>
2020-04-16 10:43:28 +1000

    added cmake

---
+++ multiscale-deshadow.cpp
@@ -0,0 +1,12 @@
+﻿// multiscale-deshadow.cpp : Defines the entry point for the application.
+//
+
+#include "multiscale-deshadow.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
